3a8ae352988348864463bf8b5ed9033d3f00cbc2
WUMANHO <wumanho@tutanota.com>
2022-05-29 00:18:07 +0800

    fetch

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-import Axios from 'axios/dist/axios';
+import {User} from "./models/User";
 
-Axios.post('http://localhost:3000/users', {
-    name: 'myname',
-    age: 20
-})
+const user = new User({id: 1, name: 'wumanho', age: 20})
+user.fetch()
 
-// import {User} from "./models/User";
-//
-// const user = new User({name: "wu", age: 20})
-// user.set({name: 'wumanho'})
-//
-// user.on('click',()=>{
-//     console.log('click 01')
-// })
-// user.on('change',()=>{
-//     console.log('change 01')
-// })
-// user.on('click',()=>{
-//     console.log('click 02')
-// })
-//
-// user.trigger('click')
-// user.trigger('change')
-//
-// console.log(user)
+
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,42 +1,52 @@
+import axios, {AxiosResponse} from 'axios/dist/axios';
+
 interface UserProps {
-    name: string,
-    age: number
+  id?: number,
+  name: string,
+  age: number
 }
 
 type TemplateUnit = string | number
 type CallBack = () => void
 type Events = {
-    [key: string]: CallBack[]
+  [key: string]: CallBack[]
 }
 
 
 export class User {
 
-    events: Events = {}
+  events: Events = {}
 
-    constructor(private data: UserProps) {
-    }
+  constructor(private data: UserProps) {
+  }
 
-    get(propName: string): TemplateUnit {
-        return this.data[propName]
-    }
+  get(propName: string): TemplateUnit {
+    return this.data[propName]
+  }
 
-    set(update: Partial<UserProps>): void {
-        Object.assign(this.data, update)
-    }
+  set(update: Partial<UserProps>): void {
+    Object.assign(this.data, update)
+  }
 
-    on(eventName: string, callback: CallBack): void {
-        // 如果不存在就初始化
-        const handlers = this.events[eventName] || []
-        handlers.push(callback)
-        this.events[eventName] = handlers
-    }
+  on(eventName: string, callback: CallBack): void {
+    // 如果不存在就初始化
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers
+  }
 
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName]
-        if (!handlers || handlers.length === 0) return
-        handlers.forEach(callback => {
-            callback()
-        })
-    }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName]
+    if (!handlers || handlers.length === 0) return
+    handlers.forEach(callback => {
+      callback()
+    })
+  }
+
+  fetch() {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((res: AxiosResponse): void => {
+        this.set(res.data)
+      })
+  }
 }
